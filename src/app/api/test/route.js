7ff34eb274dfa0965ff6781507360c3acda07376
James Holt <jamesphilipdavidholt@gmail.com>
2024-11-11 17:12:52 +0000

    adding test api route

---
+++ src/app/api/test/route.js
@@ -0,0 +1,16 @@
+// /app/api/test/route.js
+import { NextResponse } from "next/server";
+
+export async function POST() {
+	try {
+		const data = "test";
+
+		return NextResponse.json(data, { status: 200 });
+	} catch (error) {
+		console.error("Error in API:", error);
+		return NextResponse.json(
+			{ message: "Error processing request" },
+			{ status: 500 }
+		);
+	}
+}
