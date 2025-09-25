a53a80172029ac5341b03749e1a890e04b1f50fe
James Holt <jamesphilipdavidholt@gmail.com>
2025-09-25 13:26:05 +0100

    adding GTM and updating tests

--- src/app/api/processStripe/route.js
+++ src/app/api/processStripe/route.js
@@ -30,6 +30,7 @@ export async function POST(req) {
 		inspirationDetails,
 		donorType,
 		organisationName,
+		givingFrequency,
 	} = body;
 
 	const formattedGivingTo = decodeURIComponent(givingTo);
@@ -64,7 +65,7 @@ export async function POST(req) {
 			},
 		],
 		mode: "payment",
-		success_url: `${origin}/success?session_id={CHECKOUT_SESSION_ID}`,
+		success_url: `${origin}/success?currency=${currency}&amount=${amount}&gateway=stripe&frequency=${givingFrequency}&session_id={CHECKOUT_SESSION_ID}`,
 		// cancel_url: `${origin}/`,
 		payment_method_types: ["card"],
 		payment_intent_data: {
