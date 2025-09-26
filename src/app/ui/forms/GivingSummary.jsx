5219d519bec81778e6a101127d0aedc73cfe26d8
James Holt <jamesphilipdavidholt@gmail.com>
2025-09-26 12:32:19 +0100

    giving summary gift aid corrections

--- src/app/ui/forms/GivingSummary.jsx
+++ src/app/ui/forms/GivingSummary.jsx
@@ -9,7 +9,7 @@ export default function GivingSummary({
 	currency,
 }) {
 	const giftAidTotal = giftAid ? (amount * 0.25).toFixed(2) : "0.00";
-	if (currency === "usd") {
+	if (currency === "usd" || giftAid === "false") {
 		giftAid = false;
 	}
 	return (
@@ -33,6 +33,7 @@ export default function GivingSummary({
 							<div className="flex justify-between">
 								<p>Gift Aid</p>
 								<p className="font-bold">{giftAid ? "Yes" : "No"}</p>
+								<p>gift aid value = {giftAid}</p>
 							</div>
 						)}
 						<div className="flex justify-between">
